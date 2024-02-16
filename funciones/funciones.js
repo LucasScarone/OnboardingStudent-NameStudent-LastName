4df7fde514d7c91c7cda61cddab6fe5f352c6d5f
Lucas_Scarone <lucasscarone@gmail.com>
2024-02-16 11:04:22 -0300

    2

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -56,3 +56,85 @@ function perimetro(base, lados) {
 
 area(12,15)
 perimetro(12,16)
+
+// 11
+
+const calcularPrecioTotal = (precio, cantidad) => {
+    let precioTotal = precio * cantidad;
+
+    if (cantidad >= 20) {
+        precioTotal *= 0.8; // Aplicar descuento del 20%
+    } else if (cantidad >= 10) {
+        precioTotal *= 0.9; // Aplicar descuento del 10%
+    }
+
+    return precioTotal;
+};
+
+// Ejemplo de uso:
+const precioUnitario = 10;
+const cantidadProductos = 15;
+
+const precioFinal = calcularPrecioTotal(precioUnitario, cantidadProductos);
+console.log("El precio total de la compra es: $" + precioFinal);
+
+// 12
+function esMayorDeEdad (edad) {
+    if (edad >= 18) {
+        return "Eres mayor de edad";
+    } else {
+        return "Eres menor de edad";
+    }
+};
+
+
+const edadPersona = 20;
+const mensaje = esMayorDeEdad(edadPersona);
+console.log(mensaje); 
+// 
+// 13
+function calcularImpuesto(ingresoAnual) {
+    let impuesto;
+
+    if (ingresoAnual <= 10000) {
+        impuesto = ingresoAnual * 0.1; // 10% del ingreso
+    } else if (ingresoAnual > 10000 && ingresoAnual <= 20000) {
+        impuesto = ingresoAnual * 0.15; // 15% del ingreso
+    } else {
+        impuesto = ingresoAnual * 0.2; // 20% del ingreso
+    }
+
+    return impuesto;
+}
+
+const ingreso = 15000;
+const impuesto = calcularImpuesto(ingreso);
+console.log("El impuesto a pagar es: $" + impuesto);
+
+// 14
+
+function esDiaLaboral(numero) {
+    let mensaje;
+
+    switch (numero) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            mensaje = "Es un día laboral";
+            break;
+        case 6:
+        case 7:
+            mensaje = "Es fin de semana";
+            break;
+        default:
+            mensaje = "Número inválido";
+    }
+
+    return mensaje;
+}
+
+console.log(esDiaLaboral(3)); 
+console.log(esDiaLaboral(6)); 
+console.log(esDiaLaboral(8)); 
